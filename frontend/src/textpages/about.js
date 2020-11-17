4917cce28972648995f860b5781bdf03d79fcf04
nitin <ns3098@bennett.edu.in>
2020-11-17 11:50:15 +0530

    update Test

--- frontend/src/textpages/about.js
+++ frontend/src/textpages/about.js
@@ -1,25 +1,6 @@
 import React from 'react';
 import "./About.scss";
 
-import naomi from '../assets/team/naomi.png';
-import fabian_katz from '../assets/team/fabian_katz.png';
-import moritz from '../assets/team/moritz.png';
-import marco from '../assets/team/marco.png';
-import nicolai from '../assets/team/nicolai.png';
-import fabian from '../assets/team/fabian.png';
-import lea from '../assets/team/lea.png';
-import silvia from '../assets/team/silvia.png';
-import lisa from '../assets/team/lisa.png';
-import laura from '../assets/team/laura.png';
-import volker from '../assets/team/volker.png';
-import nico from '../assets/team/nico.png';
-import balint from '../assets/team/balint.png';
-import ines from '../assets/team/ines.png';
-import philipp from '../assets/team/philipp.png';
-import david from '../assets/team/david.png';
-import akolade from '../assets/team/akolade.png';
-import chiara from '../assets/team/chiara.png';
-import thomas from '../assets/team/thomas.png';
 import { Trans, useTranslation } from 'react-i18next';
 
 
